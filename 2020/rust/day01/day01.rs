0259f4a26d1de5b26bf86169e8c2496e4972f694
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-01 20:03:50 +0100

    Add solution to example

--- 2020/rust/day01/day01.rs
+++ 2020/rust/day01/day01.rs
@@ -1,3 +1,27 @@
 fn main() {
+    let report = vec![
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456,
+        // Added because otherwise solution is immediately found:
+        1,
+        2,
+        3,
+        4,
+        5,
+        1800,
+        1801,
+        1802,
+    ];
+    for front in report.iter() {
+        for back in report.iter() {
+            if front + back == 2020 {
+                println!("{} {} {}", front, back, front * back);
+            }
+        }
+    }
     println!("Hello World!");
 }
