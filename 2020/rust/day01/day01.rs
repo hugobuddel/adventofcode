11b5aef4e3e4b6c690b6977a518e6b5c94669375
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-01 21:00:10 +0100

    Generalize parse_report

--- 2020/rust/day01/day01.rs
+++ 2020/rust/day01/day01.rs
@@ -1,17 +1,17 @@
 use std::fs;
 
-fn parse_report(report: &[i32]) -> (i32, i32) {
+fn parse_report(report: &[i32], sum: i32) -> (i32, i32) {
     let mut front = 1111;
     let mut back = 2222;
     for tfront in report.iter() {
         for tback in report.iter() {
-            if tfront + tback == 2020 {
+            if tfront + tback == sum {
                 front = *tfront;
                 back = *tback;
                 break;
             }
         }
-        if tfront + back == 2020 {
+        if tfront + back == sum {
             break;
         }
     }
@@ -36,13 +36,13 @@ fn main() {
         1801,
         1802,
     ];
-    let (front, back) = parse_report(&report);
+    let (front, back) = parse_report(&report, 2020);
     println!("{} {} {} {}", front, back, front + back, front * back);
 
     // let report2: Vec<&str> = fs::read_to_string("input.txt").expect("Something went wrong reading the file.").split("\n").collect();
     let report1 = fs::read_to_string("input.txt").expect("Something went wrong reading the file.");
     let report2: Vec<&str> = report1.split("\n").collect();
     let report = report2.iter().filter(|x| **x != "").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
-    let (front, back) = parse_report(&report);
+    let (front, back) = parse_report(&report, 2020);
     println!("{} {} {} {}", front, back, front + back, front * back);
 }
