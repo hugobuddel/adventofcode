9fcc6d7f8f3570c8b8c9567a0975b870505c0d50
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-01 21:08:49 +0100

    Add solution for part 2

--- 2020/rust/day01/day01.rs
+++ 2020/rust/day01/day01.rs
@@ -18,6 +18,17 @@ fn parse_report(report: &[i32], sum: i32) -> (i32, i32) {
     (front, back)
 }
 
+fn parse_report_three(report: &[i32]) {
+    let head = &report[0];
+    let tail = &report[1..];
+    let (front, back) = parse_report(tail, 2020 - head);
+    if head + front + back == 2020 {
+        println!("Found: {} {} {} {} {}", head, front, back, head + front + back, head * front * back);
+    } else {
+        parse_report_three(tail);
+    }
+}
+
 fn main() {
     let report = vec![
         1721,
@@ -38,6 +49,7 @@ fn main() {
     ];
     let (front, back) = parse_report(&report, 2020);
     println!("{} {} {} {}", front, back, front + back, front * back);
+    parse_report_three(&report);
 
     // let report2: Vec<&str> = fs::read_to_string("input.txt").expect("Something went wrong reading the file.").split("\n").collect();
     let report1 = fs::read_to_string("input.txt").expect("Something went wrong reading the file.");
@@ -45,4 +57,5 @@ fn main() {
     let report = report2.iter().filter(|x| **x != "").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
     let (front, back) = parse_report(&report, 2020);
     println!("{} {} {} {}", front, back, front + back, front * back);
+    parse_report_three(&report);
 }
