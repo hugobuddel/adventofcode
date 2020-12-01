082af9fb87ff25a3c270d9c777be53153b08cc1e
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-01 20:58:26 +0100

    Add solution to part 1

--- 2020/rust/day01/day01.rs
+++ 2020/rust/day01/day01.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 fn parse_report(report: &[i32]) -> (i32, i32) {
     let mut front = 1111;
     let mut back = 2222;
@@ -34,6 +36,13 @@ fn main() {
         1801,
         1802,
     ];
+    let (front, back) = parse_report(&report);
+    println!("{} {} {} {}", front, back, front + back, front * back);
+
+    // let report2: Vec<&str> = fs::read_to_string("input.txt").expect("Something went wrong reading the file.").split("\n").collect();
+    let report1 = fs::read_to_string("input.txt").expect("Something went wrong reading the file.");
+    let report2: Vec<&str> = report1.split("\n").collect();
+    let report = report2.iter().filter(|x| **x != "").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
     let (front, back) = parse_report(&report);
     println!("{} {} {} {}", front, back, front + back, front * back);
 }
