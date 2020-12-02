67edc0f7b8097a9b670d8b701695819f14a628c3
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-02 09:58:53 +0100

    Day2 template

---
+++ 2020/rust/day02/day02.rs
@@ -0,0 +1,43 @@
+// All warnings enabled, to learn what they do.
+#![warn(
+    absolute_paths_not_starting_with_crate,
+    anonymous_parameters,
+    box_pointers,
+    clashing_extern_declarations,
+    deprecated_in_future,
+    elided_lifetimes_in_paths,
+    explicit_outlives_requirements,
+    indirect_structural_match,
+    keyword_idents,
+    macro_use_extern_crate,
+    meta_variable_misuse,
+    missing_copy_implementations,
+    missing_crate_level_docs,
+    missing_debug_implementations,
+    missing_docs,
+    missing_doc_code_examples,
+    non_ascii_idents,
+    private_doc_tests,
+    single_use_lifetimes,
+    trivial_casts,
+    trivial_numeric_casts,
+    unaligned_references,
+    unreachable_pub,
+    unsafe_code,
+    // unsafe_op_in_unsafe_fn,
+    unstable_features,
+    unused_crate_dependencies,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences,
+)]
+
+//! Advent of Code 2020 Day 2
+
+
+fn main() {
+    println!("Advent of Code 2020 Day 2");
+}
