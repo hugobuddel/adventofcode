2ac807428af2d121ef10d4ae0037bf12e031d3aa
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-07 20:02:47 +0100

    Minimal problem

---
+++ 2020/rust/test07a/src/main.rs
@@ -0,0 +1,27 @@
+
+fn main () {
+    println!("Advent of Code 2020 Day 7.");
+
+    let line = "abcd efgh";
+    let mut mylines: Vec<&str> = Vec::new();
+    for word in line.split(" ") {
+        let word2 = word.replace("a", "b");
+        mylines.push(&word2);
+    }
+    println!("{:?}", mylines);
+}
+
+// error[E0597]: `word2` does not live long enough
+//   --> src/main.rs:9:22
+//    |
+// 9  |         mylines.push(&word2);
+//    |                      ^^^^^^ borrowed value does not live long enough
+// 10 |     }
+//    |     - `word2` dropped here while still borrowed
+// 11 |     println!("{:?}", mylines);
+//    |                      ------- borrow later used here
+//
+// error: aborting due to previous error
+//
+// For more information about this error, try `rustc --explain E0597`.
+// error: could not compile `test07a`.
