638edf9383e1284ffd77a6fe3570296b3ee6fde8
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-13 20:06:35 +0100

    Add cvs.pest, wrong name though..

--- 2020/rust/test12a/src/main.rs
+++ 2020/rust/test12a/src/main.rs
@@ -2,6 +2,12 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "cvs.pest"]
+pub struct CSVParser;
+
 fn main() {
     println!("Hello, world!");
 }
