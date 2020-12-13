8304fc8c9a66750d901bfc3ea179e85a55cf813e
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-13 20:10:03 +0100

    Add first parse!

--- 2020/rust/test12a/src/main.rs
+++ 2020/rust/test12a/src/main.rs
@@ -10,4 +10,12 @@ pub struct CSVParser;
 
 fn main() {
     println!("Hello, world!");
+
+    let successful_parse = CSVParser::parse(Rule::field, "-273.15");
+    println!("{:?}", successful_parse);
+
+    let unsuccessful_parse = CSVParser::parse(Rule::field, "hello");
+    println!("{:?}", unsuccessful_parse);
+
+
 }
