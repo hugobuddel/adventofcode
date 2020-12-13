b1b9dce1a20ffa53fba7063f5a4f4263c8c8d544
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-12-13 20:20:17 +0100

    Add example parser

--- 2020/rust/test12a/src/main.rs
+++ 2020/rust/test12a/src/main.rs
@@ -4,6 +4,8 @@ extern crate pest_derive;
 
 use pest::Parser;
 
+use std::fs;
+
 #[derive(Parser)]
 #[grammar = "cvs.pest"]
 pub struct CSVParser;
@@ -17,5 +19,29 @@ fn main() {
     let unsuccessful_parse = CSVParser::parse(Rule::field, "hello");
     println!("{:?}", unsuccessful_parse);
 
+    let unparsed_file = fs::read_to_string("numbers.csv").expect("Oops");
 
+    let file = CSVParser::parse(Rule::file, &unparsed_file)
+        .expect("Unsuccessful parse")
+        .next().unwrap();
+
+    let mut field_sum: f64 = 0.0;
+    let mut record_count: u64 = 0;
+
+    for record in file.into_inner() {
+        match record.as_rule() {
+            Rule::record => {
+                record_count += 1;
+
+                for field in record.into_inner() {
+                    field_sum += field.as_str().parse::<f64>().unwrap();
+                }
+            }
+            Rule::EOI => (),
+            _ => unreachable!(),
+        }
+    }
+
+    println!("Sum of fields: {}", field_sum);
+    println!("Number of records: {}", record_count);
 }
