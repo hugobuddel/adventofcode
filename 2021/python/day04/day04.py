4804cf430979ca3b49252f10113adcd43ea29e24
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2021-12-05 20:36:00 +0100

    Solve example

--- 2021/python/day04/day04.py
+++ 2021/python/day04/day04.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import sys
 
 fn = "example.txt"
 
@@ -21,7 +22,55 @@ def parse_board(board):
     ]
     return board3
 
+
 boards1 = [board for board in data2[1:] if board]
 boards2 = [parse_board(board) for board in boards1]
 boards = boards2
 pprint(boards)
+
+
+def check_number(board, number):
+    board = [
+        [
+            -1 if cell == number else cell
+            for cell in line
+        ]
+        for line in board
+    ]
+    return board
+
+
+def check_board_lines(board):
+    for line in board:
+        if sum(line) == -5:
+            return True
+    return False
+
+
+def check_board(board):
+    board_trans = list(zip(*board))
+    if check_board_lines(board):
+        return True
+    if check_board_lines(board_trans):
+        return True
+    return False
+
+
+for number in numbers:
+    print("Drawing", number)
+    pprint(boards)
+    boards_old = boards
+    boards = []
+    for board in boards_old:
+        board = check_number(board, number)
+        boards.append(board)
+        if check_board(board):
+            board2 = [
+                0 if cell == -1 else cell
+                for line in board
+                for cell in line
+            ]
+            sum_board = sum(board2)
+            prod = sum_board * number
+            print(number, sum_board, prod)
+            sys.exit()
