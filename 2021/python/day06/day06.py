228a680d736627a72abbf5d7fecf8ccefd082e55
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2021-12-06 12:11:42 +0100

    Solve part 2

--- 2021/python/day06/day06.py
+++ 2021/python/day06/day06.py
@@ -11,11 +11,12 @@ population = {
 }
 pprint(population)
 
-for day in range(80):
+for day in range(256):
     babies = population[0]
     for age in range(1, age_max):
         population[age-1] = population[age]
     population[8] = babies
     population[6] += babies
     pop_tot = sum(population.values())
-    pprint((day, population, pop_tot))
+    if day in (79, 255):
+        pprint((day, population, pop_tot))
