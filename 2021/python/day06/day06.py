c4ade8d564860281537dcc5576e06192a5fee8d9
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2021-12-06 12:09:32 +0100

    Example

---
+++ 2021/python/day06/day06.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+fn = "example.txt"
+data = [int(c) for c in open(fn).read().strip().split(",")]
+age_max = max(max(data), 9)
+
+population = {
+    age: data.count(age)
+    for age in range(age_max)
+}
+pprint(population)
+
+for day in range(80):
+    babies = population[0]
+    for age in range(1, age_max):
+        population[age-1] = population[age]
+    population[8] = babies
+    population[6] += babies
+    pop_tot = sum(population.values())
+    pprint((day, population, pop_tot))
