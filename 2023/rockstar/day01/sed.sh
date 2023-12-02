db13903341f9a4862c2c237840b2f2970bb9dcea
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-12-02 01:38:31 +0100

    Add cheating workaround!

---
+++ 2023/rockstar/day01/sed.sh
@@ -0,0 +1,11 @@
+cp -av puzzle.txt puzzle.sed.txt
+sed -i 's/one/one1one/g' puzzle.sed.txt
+sed -i 's/two/two2two/g' puzzle.sed.txt
+sed -i 's/three/three3three/g' puzzle.sed.txt
+sed -i 's/four/four4four/g' puzzle.sed.txt
+sed -i 's/five/five5five/g' puzzle.sed.txt
+sed -i 's/six/six6six/g' puzzle.sed.txt
+sed -i 's/seven/seven7seven/g' puzzle.sed.txt
+sed -i 's/eight/eight8eight/g' puzzle.sed.txt
+sed -i 's/nine/nine9nine/g' puzzle.sed.txt
+
